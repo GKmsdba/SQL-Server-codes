0b162b846673a8e225ecf090146fe0d5976ae434
Puneet Chugh <puneet.chugh@nyu.edu>
2017-01-19 21:43:16 -0500

    Attach Command for AdventureWorks database

---
+++ AttachCommand.sql
@@ -0,0 +1,15 @@
+
+
+
+use [master]
+Go
+Create database[AdventureWorks]
+on
+(Filename= N'C:\Program Files\Microsoft SQL Server\MSSQL13.DEV01\MSSQL\DATA\AdventureWorks_Data.mdf' )
+for attach
+
+
+
+
+
+
